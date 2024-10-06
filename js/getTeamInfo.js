07d40c607fbe1942508565826c0710c3caad50f3
tomanaleksiev <tomanaleksiev101@gmail.com>
2024-10-06 15:12:00 -0400

    finished getTeamPlayer and added on-event

--- js/getTeamInfo.js
+++ js/getTeamInfo.js
@@ -8,3 +8,35 @@ function getTeamName(n) {
 		throw "Team number not 1 or 2";
 	}
 }
+
+function getTeamPlayers(n) {
+	var teamNum = "";
+	var teamPlayers = [];
+
+	if (n == 1) {
+		teamNum = "team-one";
+	} else if (n == 2) {
+		teamNum = "team-two";
+	} else {
+		throw "Team number not 1 or 2";
+	}
+	$("div." + teamNum + " table tbody tr").each(function(index) {
+		var dic = {};
+
+		var tdNum = $(this).children("td")[0];
+		var tdName = $(this).children("td")[1];
+		var tdSub = $(this).children("td")[2];
+
+		var numVal = $(tdNum).children().val();
+		var nameVal = $(tdName).children().val();
+		var subVal = $(tdSub).children().is(":checked");
+
+		if (numVal && nameVal) {
+			dic["Number"] = numVal;
+			dic["Name"] = nameVal;
+			dic["Sub"] = subVal;
+			teamPlayers.push(dic);
+		}
+	});
+	return teamPlayers;
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 var teamNames = ["", ""];
+var teamPlayers = [[], []];
 
-$('div.team-one > div.team-name > p > input').on('input',function(e){
+$('div.team-one > div.team-name > p > input').on('input', function(){
 	teamNames[0] = getTeamName(1);
-	console.log(teamNames);
+	// console.log(teamNames);
 });
 
-$('div.team-two > div.team-name > p > input').on('input',function(e){
+$('div.team-two > div.team-name > p > input').on('input', function(){
 	teamNames[1] = getTeamName(2);
-	console.log(teamNames);
+	// console.log(teamNames);
+});
+
+$('div.team-one > table > tbody > tr > td > input').on('input', function(){
+	teamPlayers[0] = getTeamPlayers(1);
+	// console.log(teamPlayers);
+});
+
+$('div.team-two > table > tbody > tr > td > input').on('input', function(){
+	teamPlayers[1] = getTeamPlayers(2);
+	// console.log(teamPlayers);
 });

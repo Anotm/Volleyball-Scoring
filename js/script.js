d5d11f15a291880a381b04f38c4ff5da59613bfc
Anotm <tomanaleksiev101@gmail.com>
2024-10-07 18:00:18 -0400

    started auto fill other team name

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ var teamPlayers = [[], []];
 
 $('div.team-one > div.team-name > p > input').on('input', function(){
 	teamNames[0] = getTeamName(1);
+	updateTeamNameDropDown(teamNames);
 	// console.log(teamNames);
 });
 
 $('div.team-two > div.team-name > p > input').on('input', function(){
 	teamNames[1] = getTeamName(2);
+	updateTeamNameDropDown(teamNames);
 	// console.log(teamNames);
 });
 
@@ -19,4 +21,8 @@ $('div.team-one > table > tbody > tr > td > input').on('input', function(){
 $('div.team-two > table > tbody > tr > td > input').on('input', function(){
 	teamPlayers[1] = getTeamPlayers(2);
 	// console.log(teamPlayers);
+});
+
+$("select.select-team-name").on("change", function(){
+	setOtherTeamName(this);
 });
--- js/updateDropDowns.js
+++ js/updateDropDowns.js
@@ -1,3 +1,17 @@
-function updateTeamNameDrppDown() {
-	// body...
+function updateTeamNameDropDown(teamNameList) {
+	const team1Name = teamNameList[0];
+	const team2Name = teamNameList[1];
+	$("select.select-team-name option:nth-child(2)").attr("value", team1Name);
+	$("select.select-team-name option:nth-child(3)").attr("value", team2Name);
+
+	$("select.select-team-name option:nth-child(2)").text(team1Name);
+	$("select.select-team-name option:nth-child(3)").text(team2Name);
+}
+
+function setOtherTeamName(el) {
+	const parentTable = $(el).parent().parent().parent().parent();
+	const parentTableClass = $(parentTable).attr("class");
+	const parentSet = $(parentTable).parent();
+	console.log(parentTable);
+	console.log(parentSet);
 }
